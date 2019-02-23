6e029b4715068f19be50b6421e3f2c43f9ab0304
Aliaksandr Chahrynets <AlexGreen2015@icloud.com>
2019-02-23 01:35:55 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  host: "ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "pz4oq3sbc9ml3gf0",
+  password: "szsd01lh4av75cle",
+  database: "z1i0d97eggh32725",
+  port:"3306"
 });
 
 connection.connect(function(err) {
